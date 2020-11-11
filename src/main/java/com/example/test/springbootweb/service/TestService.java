58f2b0122e416251a16e0419e88041f6458e8299
zhangrong <39478871+brianzhangrong@users.noreply.github.com>
2020-11-11 17:36:01 +0800

    init

--- src/main/java/com/example/test/springbootweb/service/TestService.java
+++ src/main/java/com/example/test/springbootweb/service/TestService.java
@@ -2,7 +2,13 @@ package com.example.test.springbootweb.service;
 
 import com.example.test.springbootweb.constant.Version;
 import com.example.test.springbootweb.dao.TestDao;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 import javax.annotation.Resource;
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -13,9 +19,27 @@ public class TestService {
 
     @Resource
     TestDao testDao;
+    private static OkHttpClient okHttpClient = new OkHttpClient.Builder()
+            .connectTimeout(5, TimeUnit.SECONDS)
+            .readTimeout(60, TimeUnit.SECONDS)
+            .writeTimeout(60, TimeUnit.SECONDS)
+            .build();
 
     public String version(){
         log.info("web2 service {}",Version.VERSION);
-        return "web2 svc-"+ Version.VERSION+","+testDao.version();
+        Request getRequest =
+                new Request.Builder().get().url("http://web3:8080/test").build();
+        Call getCall = okHttpClient.newCall(getRequest);
+        Response execute = null;
+        try {
+            execute = getCall.execute();
+            if(execute.isSuccessful()){
+                return "web2 svc-"+ Version.VERSION+","+testDao.version()+"--->"+execute.body().string();
+            }
+        } catch (IOException e) {
+
+        }
+        return "web3 error";
+
     }
 }
